a8401d54beafe89418ec7c898d44ff52f461dd14
ffding <dff@pku.edu.cn>
2022-10-24 00:42:04 +0000

    init

---
+++ parse_ap.py
@@ -0,0 +1,56 @@
+import openpyxl
+import json
+import re
+import zhconv
+from pypinyin import lazy_pinyin
+
+workbook = openpyxl.load_workbook("data/rubbing.xlsx")
+# workbook = openpyxl.load_workbook("data/METADATA.xlsx")
+shenames = workbook.sheetnames
+# print(shenames)
+
+worksheet = workbook["Result 1"]
+# print(worksheet) 
+
+rows = worksheet.max_row
+columns = worksheet.max_column
+# print(rows, columns) #3
+
+data = {}
+
+first_row = True
+idx = 0
+
+for row in worksheet.rows: 
+    if first_row:
+        first_row = False
+        continue
+    else:
+        xml = row[1].value
+        res = re.findall(r"title titleAttribute=\"(.*?)\"", xml)
+        # res = re.findall(r"\<gravureDesc\>(.*?)\<\/gravureDesc\>", xml)
+
+        # res = re.findall(r"historyattribute=\"\"", xml)
+        # if len(res) == 0:
+        #     continue
+
+        # res = re.findall(r"\<calis:titlevalue\>太白酒(.+?)\<\/calis:titlevalue\>", xml)
+        # res = re.findall(r"\<calis:title titleattribute=\"正題名及説明\"\>(.+?)\<\/calis:title\>", xml)
+        # if len(res) > 0:
+        #     print(row[0].value, xml)
+
+        if len(res) > 1:
+            idx += 1
+        if len(res) > 0:
+            for str in res:
+                # str = str.strip()
+                # str = hant_2_hans(str)
+                # str = ''.join(lazy_pinyin(str))
+                data.setdefault(str, 0)
+                data[str] += 1
+
+for key in data.keys():
+    print(key, data[key])
+
+print(idx)
+        
---
+++ parse_json.py
@@ -0,0 +1,51 @@
+# 先清洗数据，仅留下jsonb的action为checkedout的条目
+
+import openpyxl
+import json
+
+workbook = openpyxl.load_workbook("data/chi0706.xlsx")
+shenames = workbook.sheetnames
+# print(shenames)
+
+worksheet = workbook["Sheet1"]
+# print(worksheet) 
+
+rows = worksheet.max_row
+columns = worksheet.max_column
+# print(rows, columns) #46854 73
+
+
+cn_attr = ["出版年", "作者", "版本", "图书装帧", "语种", "卷", "页码", "价格", "系列", "出版社", "ISBN10", "ISBN13", "币种"]
+en_attr = ["publish_year", "author", "edition", "binding", "language", "volume", "pages", "price", "series", "publish", "ISBN10", "ISBN13", "currency"]
+clean_rows = []
+first_row = True
+
+clean_worksheet = workbook.create_sheet() 
+clean_worksheet.title = "Parse_Result"
+
+row_idx = 0
+for row in worksheet.rows: 
+    clean_row = []
+    for cell_idx in range(len(row)):
+        if cell_idx == 14 or cell_idx == 17:
+            continue
+        cell = row[cell_idx]
+        clean_row.append(cell.value)
+    if first_row:
+        first_row = False
+        for idx in range(len(cn_attr)):
+            clean_row.append(cn_attr[idx]+"("+en_attr[idx]+")")
+    else:
+        jsonb = json.loads(row[17].value)["attrValueMap"]
+        for idx in range(len(cn_attr)):
+            if en_attr[idx] in jsonb:
+                attr_value = jsonb[en_attr[idx]]["value"]
+            else:
+                attr_value = ""
+            clean_row.append(attr_value)
+    print(clean_row)
+    for col_idx in range(len(clean_row)):
+        clean_worksheet.cell(row_idx+1, col_idx+1, clean_row[col_idx])
+    row_idx = row_idx + 1
+
+workbook.save(filename='data/b.xlsx')
